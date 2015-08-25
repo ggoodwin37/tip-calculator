af0262af7975bf4cecd0430e0c1f85153ad24fde
gideong <gideong@yahoo-inc.com>
2015-08-24 19:16:22 -0700

    Took me way to fucking long to get to this point.

---
+++ tip-calculator/SettingsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsViewController.swift
+//  tip-calculator
+//
+//  Created by Gideon Goodwin on 8/24/15.
+//  Copyright © 2015 Gideon Goodwin. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    @IBAction func onTouchUpInside(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
