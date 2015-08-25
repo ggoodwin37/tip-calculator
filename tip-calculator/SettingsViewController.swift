132a36b31b11b5f2f366a2d2c256615b291415f1
gideong <gideong@yahoo-inc.com>
2015-08-24 21:02:01 -0700

    Adding segment control to settings.

--- tip-calculator/SettingsViewController.swift
+++ tip-calculator/SettingsViewController.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    @IBOutlet weak var tipControl: UISegmentedControl!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
     @IBAction func onTouchUpInside(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    @IBAction func onTipControlChanged(sender: AnyObject) {
+        let foo = tipControl.selectedSegmentIndex
+        print("\(foo)")
     }
 }
