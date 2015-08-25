ff43751b717bf55be2c71caa0a91a074d1873c25
gideong <gideong@yahoo-inc.com>
2015-08-24 17:40:50 -0700

    Adding connections.

--- tip-calculator/ViewController.swift
+++ tip-calculator/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billAmountTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
